99f05fe43426206ac4f4dd3db444b213bd85928a
Minato-namikaze99 <im.indrajit9954@gmail.com>
2023-06-12 19:31:25 +0530

    fixing some bugs

--- conc_proc_10_04_2023/file.c
+++ conc_proc_10_04_2023/file.c
@@ -1,39 +1,35 @@
 #include<stdio.h>
 #include<unistd.h>
 #include<string.h>
+#include<sys/wait.h>
+#include<fcntl.h>
 
 int main()
 {
-	int ar[2], arr[2], x, id;
+	int ar[2],x, id;
 	x=pipe(ar);
 
 	if (x==0)
 	{
-		id=fork();
+		id =fork();
 		if (id==0)
 		{
+			printf("\nCHILD PROCESS\n");
 			printf("Enter a string - ");
-			char buf1[100];
-			scanf("%[^\n]", buf1);
-
-			int l=strlen(buf1);
-			printf("\n Child Process");
-			write(ar[1], 1, 4);
-
-			write(ar[1], buf1, l);
-			//ar[1] is used to write values while ar[0] is used to read from it
+			char buf[100];
+			scanf("%[^\n]", buf);
+			write(ar[1], buf, 20);
 		}
 
-		else
+		else 
 		{
-			printf("\n Parent Process");
-			int l1;
-			char buf[10];
-			read(ar[0], l1, 4);
-			read(ar[0], buf, l1);
-
-			printf("\n The received data is - %s\n", buf);
+			wait(NULL);
+			printf("\nPARENT PROCESS\n");
+			char buf[100];
+			read(ar[0], buf, 20);
+			printf("The received string is - %s\n", buf);
 		}
 	}
+
 	return 0;
 }
